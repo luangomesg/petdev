0a406cb6a85e6ad55ffd94240365f719c34cfde2
Luan <luan.gomesdv@gmail.com>
2026-09-24 02:49:32 -0300

    our services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "./_components/about";
 import Hero from "./_components/hero";
+import OurServices from "./_components/ourServices";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <OurServices />
     </main>
   );
 }
